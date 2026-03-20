8befc91504c9113f38bfcf1487d54c5d6cd70c50
Ayush Sharma <ayush.sharmaa@hotmail.com>
2026-03-20 11:14:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',   // Netlify serves from root — no subdirectory needed
+  base: '/NexaSphere/',
 })
